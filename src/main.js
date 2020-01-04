ea92a7c632ab295d92a356ac3c50c2b865113bcb
shit.mail@icloud.com <michaelblack@Michaels-MBP.fios-router.home>
2020-01-04 00:25:47 -0500

    vuetify upgrade

--- src/main.js
+++ src/main.js
@@ -1,20 +1,56 @@
+// import firebase from 'firebase'
+// import Vue from 'vue'
+// import './plugins/vuetify'
+// import App from './App.vue'
+// import router from './router'
+// import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+// import { store } from './store/store'
+
+// Vue.config.productionTip = false
+
+
+
+// let app = ''
+// // This is a git test.
+// firebase.auth().onAuthStateChanged(() => {
+//   if (!app) {
+//     app = new Vue({
+//       router,
+//       store: store,
+//       render: h => h(App)
+//     }).$mount('#app')
+//   }
+// });
+
+// Old implementation::  The above implementation keeps user signed in after refresh
+// new Vue({
+//   router,
+//   store: store,
+//   render: h => h(App)
+// }).$mount('#app')
+
 import firebase from 'firebase'
 import Vue from 'vue'
-import './plugins/vuetify'
 import App from './App.vue'
+import './plugins/vuetify'
 import router from './router'
-import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
-import { store } from './store/store'
+// import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+import '@mdi/font/css/materialdesignicons.css' // update with vuetify 2.0
 
+import { store } from './store/store'
+import vuetify from './plugins/vuetify';
 
 
 Vue.config.productionTip = false
+
+
 
 let app = ''
 // This is a git test.
 firebase.auth().onAuthStateChanged(() => {
   if (!app) {
     app = new Vue({
+      vuetify,
       router,
       store: store,
       render: h => h(App)
@@ -22,9 +58,16 @@ firebase.auth().onAuthStateChanged(() => {
   }
 });
 
-// Old implementation::  The above implementation keeps user signed in after refresh
+// *******
+
+// // src/main.js
+// import Vue from 'vue'
+// import App from './App.vue'
+// import vuetify from './plugins/vuetify';
+
+// Vue.config.productionTip = false
+
 // new Vue({
-//   router,
-//   store: store,
+//   vuetify,
 //   render: h => h(App)
 // }).$mount('#app')
